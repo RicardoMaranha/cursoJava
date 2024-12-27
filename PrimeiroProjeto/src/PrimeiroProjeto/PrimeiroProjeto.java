0e2e3330a850d1fdfadb8fb36663429d2c2c62e0
RicardoMaranha <ricardomaranhapro@outlook.com>
2024-12-27 13:55:12 -0300

    Edição cod 1

--- PrimeiroProjeto/src/PrimeiroProjeto/PrimeiroProjeto.java
+++ PrimeiroProjeto/src/PrimeiroProjeto/PrimeiroProjeto.java
@@ -3,6 +3,7 @@ package PrimeiroProjeto;
 public class PrimeiroProjeto {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!!!");
+		System.out.println("Segunda Linha de código e git");
 	}
 	
 }
